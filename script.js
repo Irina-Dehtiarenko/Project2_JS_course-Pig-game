44ae98eb9707ea31ef7dac03291a05dfa454631d
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-11-14 12:06:14 +0100

    created the separate function for switching current player

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const currentScore0El = document.getElementById('current--0');
-const currentScore1El = document.getElementById('current--1');
-const player0 = document.querySelector('.player--0');
-const player1 = document.querySelector('.player--1');
+// const currentScore0El = document.getElementById('current--0');
+// const currentScore1El = document.getElementById('current--1');
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 const diceEl = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
@@ -22,6 +23,17 @@ let activePlayer = 0;
 let currentScore = 0;
 // console.log(dice);
 
+const switchPlayer = () => {
+  currentScore = 0;
+
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+  activePlayer = activePlayer === 0 ? 1 : 0;
+};
+
 const randomDice = () => {
   const dice = Math.trunc(Math.random() * 6) + 1;
   diceEl.src = `./img/dice-${dice}.png`;
@@ -29,31 +41,23 @@ const randomDice = () => {
 
   if (dice !== 1) {
     currentScore += dice;
-    console.log(currentScore);
 
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
-
-    // change later
-    //    currentScore0El.textContent = currentScore;
   } else {
-    currentScore = 0;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-
-    if (activePlayer === 0) {
-      activePlayer = 1;
-    } else {
-      activePlayer = 0;
-    }
-    console.log(currentScore);
-
-    // change later
-    // currentScore0El.textContent = currentScore;
-
-    player0.classList.toggle('player--active');
-    player1.classList.toggle('player--active');
+    switchPlayer();
   }
 };
-console.log(currentScore);
+
+const addScore = () => {
+  scores[activePlayer] += currentScore;
+
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  switchPlayer();
+};
+
 btnRoll.addEventListener('click', randomDice);
+
+btnHold.addEventListener('click', addScore);
