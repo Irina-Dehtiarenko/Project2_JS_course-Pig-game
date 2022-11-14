8f929315e86407ff920207314d3254e887ac999a
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2022-11-14 10:43:58 +0100

    added  function with random dice and show it in the picture

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const currentScore0El = document.getElementById('current--0');
+const currentScore1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--newl');
+const btnHold = document.querySelector('.btn--hold');
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+let currentScore = 0;
+// console.log(dice);
+
+const randomDice = () => {
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.src = `./img/dice-${dice}.png`;
+  diceEl.classList.remove('hidden');
+};
+console.log(currentScore);
+btnRoll.addEventListener('click', randomDice);
